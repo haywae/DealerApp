c00ab4c275ca21270b0e9620907cea3f4a10fdde
haywae <haywae35@gmail.com>
2023-10-04 16:45:06 +0300

    Line Break added and Website name corrected

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,12 +6,12 @@ export function About(){
             <Helmet>
                 <title>About - WolexChange</title>
             </Helmet>
-            <h2 className="about-head-2">Wolexchange</h2>
+            <h2 className="about-head-2">WolexChange</h2>
             <p className="about-p">
-                Wolexchange is a platform dedicated to providing the best tools and environment for financial services, 
-                with a focus on dependability. We work hard to transform our passion in fintech into a thriving online website. 
+                WolexChange is a platform dedicated to providing the best tools for individuals and organisations involved in financial 
+                services, with a focus on dependability. We work hard to transform our passion in fintech into a thriving online website. 
                 We hope you enjoy our services as much as we enjoy offering them to you. 
-                Thank you for visiting our Website.
+                <br />Thank you for visiting our Website.
             </p>
 
         </div>
